743c09c248704a346395964eb73bc5dee1ece841
ssohy <sebastiensohy343@gmail.com>
2020-02-01 00:53:22 -0500

    Integrated Shoot event

--- _main/Main_GameJam/Assets/Scripts/PlayerInputs.cs
+++ _main/Main_GameJam/Assets/Scripts/PlayerInputs.cs
@@ -32,6 +32,8 @@ public class PlayerInputs : MonoBehaviour, IFlow
 
         if (player.GetButtonDown("Shoot"))
         {
+            playerEvents.Throw();
+            
             Debug.Log("Shooting");
         }
 
--- _main/Main_GameJam/Assets/Scripts/playerEvents/PlayerEvents.cs
+++ _main/Main_GameJam/Assets/Scripts/playerEvents/PlayerEvents.cs
@@ -19,9 +19,13 @@ public class PlayerEvents : CustomEventBehaviour<PlayerEvents.Event>
     [SerializeField] private CustomEvent onThrow;
     public float speed = 100;
     public Rigidbody rb;
+    public GameObject bullet;
+    public Transform shotSpawn;
     private void PreInitialize()
     {
+        
         rb = GetComponent<Rigidbody>();
+        bullet = Resources.Load<GameObject>("Prefabs/NotBullet");
         onDash = new CustomEvent();
         onDie = new CustomEvent();
         onThrow = new CustomEvent();
@@ -70,6 +74,9 @@ public class PlayerEvents : CustomEventBehaviour<PlayerEvents.Event>
 
     public void Throw()
     {
+
+        GameObject shot = Instantiate(bullet, shotSpawn);
+        shot.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(100,0,0),ForceMode.Force);
         Debug.Log("In Throw");
 
     }
