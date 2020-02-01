f70b61ad6b3e0e71249adf9c6403dc8e8fc24655
zammorrak <46572000+zammorrak@users.noreply.github.com>
2020-01-31 22:28:29 -0500

    beginUi

---
+++ _main/Main_GameJam/Assets/Scripts/UiManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : IFlow
+{
+    // Start is called before the first frame update
+
+    public void PreInitialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Initialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Refresh()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void PhysicsRefresh()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void LateRefresh()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void EndFlow()
+    {
+        throw new System.NotImplementedException();
+    }
+}
