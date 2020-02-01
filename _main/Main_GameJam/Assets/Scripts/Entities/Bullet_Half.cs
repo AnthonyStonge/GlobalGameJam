8f4e76d5af24395c86a868f55632f1c92298fb38
Gabriel Alexandre Desgagné <gdesgagn@isimtl.local>
2020-02-01 00:21:07 -0500

    Egg Split

---
+++ _main/Main_GameJam/Assets/Scripts/Entities/Bullet_Half.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Bullet_Half : MonoBehaviour, IPickable
+{
+    private Rigidbody rb;
+
+    public float timeBeforeFrozen = 5f;
+
+    private void Awake()
+    {
+        TimeManager.Instance.PreInitialize();
+        //TODO remove line under
+    }
+
+    private void Start()
+    {
+        Initialize();
+    }
+
+    private void Update()
+    {
+        TimeManager.Instance.Refresh();
+    }
+
+    public void Initialize()
+    {
+        this.rb = GetComponent<Rigidbody>();
+        TimeManager.Instance.AddTimedAction(new TimedAction(() => { this.rb.isKinematic = true; }, this.timeBeforeFrozen));
+    }
+
+
+    public void PickedUp()
+    {
+        
+    }
+}
