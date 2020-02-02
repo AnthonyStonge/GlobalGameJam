73490e89c67d3af429804250ca06830299129d41
marcuslelus <44736661+marcuslelus@users.noreply.github.com>
2020-02-02 13:10:01 -0500

    RANDOM CHICKS, RANDOM CHILDCARE

---
+++ _main/Main_GameJam/Assets/ChickJump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator anim;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(EnableJump());
+    }
+
+    // Update is called once per frame
+    public IEnumerator EnableJump()
+    {
+        yield return new WaitForSeconds(Random.Range(0, 5f));
+        anim.SetTrigger("Jump");
+        
+    }
+}
